8600dbff68a69409e0b259664bad3ee1e1256fd2
2000Eduardo <eduardo.gomez@ucb.edu.bo>
2025-08-28 10:03:56 -0400

    Adiciono el ejercicio 02 de la practica.

---
+++ PRACTICA_01/Ejercicio_01_02.cpp
@@ -0,0 +1,31 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Eduardo Gomez Rojas
+// Carnet: 11104404 L.P.
+// Carrera del estudiante: Diseño Digital
+// Fecha creación: 28/08/2025
+// Número de ejercicio: 2
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    
+    int variable_1 = 0;
+    int variable_2 = 0;
+
+    cout << "Valores iniciales:" << endl;
+    cout << "variable 1: " << variable_1 << endl;
+    cout << "variable 2: " << variable_2 << endl;
+
+    cout << "Ingresa un valor para variable 1: ";
+    cin >> variable_1;
+    cout << "Ingresa un valor para variable 2: ";
+    cin >> variable_2;
+
+    cout << "Valores ingresados:" << endl;
+    cout << "variable 1: " << variable_1 << endl;
+    cout << "variable 2: " << variable_2 << endl;
+
+    return 0;
+}
